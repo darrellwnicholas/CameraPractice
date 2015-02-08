5b634e65b5696ae54cf82527062f60702697f23f
Darrell Nicholas <darrellwnicholas@gmail.com>
2015-02-07 19:26:08 -0600

    Initial Commit

---
+++ CameraPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CameraPractice
+//
+//  Created by Darrell Nicholas on 2/7/15.
+//  Copyright (c) 2015 Darrell Nicholas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
