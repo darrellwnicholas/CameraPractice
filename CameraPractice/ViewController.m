fc8033c01f040c7d1dbedb29ec9436eba1259245
Darrell Nicholas <darrellwnicholas@gmail.com>
2015-02-07 22:33:51 -0600

    initial commit. This works fine for taking a photo, converting it to png, giving it a uuid string name with png at the end and saves it in the documents directory. I logged out the results so that I could get the actual url of the image and load it during app launch and it worked. I assume what I will need to do is in the pickerView delegate method when the user takes a photo, grab a url by creating a path and a uuid string at the same time and commit it to the realm database. Remember, you are only saving the url string to the database, not the actual image. This also does not add the images from the app to the photos app. You will want to create some way of deleting images from the url strings in the main app.

--- CameraPractice/ViewController.m
+++ CameraPractice/ViewController.m
@@ -16,12 +16,66 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSString *uuidImageName = @"<__NSConcreteUUID 0x170229cc0> F1CE2B5A-D740-48AD-B22E-DCCF03E3C3E0.png";
     // Do any additional setup after loading the view, typically from a nib.
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsPath = [paths objectAtIndex:0]; //Get the docs directory
+    //NSString *uuidImageName = [NSString stringWithFormat:@"/images/%@.png",[NSUUID UUID]];
+    NSString *filePath = [documentsPath stringByAppendingPathComponent:uuidImageName]; //Add the file name
+    NSData *pngData = [NSData dataWithContentsOfFile:filePath];
+    UIImage *loadImage = [UIImage imageWithData:pngData];
+    _imageView.image = loadImage;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)takePhoto:(UIButton *)sender {
+    
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    
+    [self presentViewController:picker animated:YES completion:NULL];
+}
+
+- (IBAction)selectPhoto:(UIButton *)sender {
+    
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    
+    [self presentViewController:picker animated:YES completion:NULL];
+}
+
+#pragma mark - UIImagePickerController Delegate Methods
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    
+    UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
+    // turn the image into png data
+    NSData *pngData = UIImagePNGRepresentation(chosenImage);
+    
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsPath = [paths objectAtIndex:0]; //Get the docs directory
+    NSString *uuidImageName = [NSString stringWithFormat:@"%@.png",[NSUUID UUID]];
+    NSString *filePath = [documentsPath stringByAppendingPathComponent:uuidImageName]; //Add the file name
+    NSLog(@"%@", filePath);
+    [pngData writeToFile:filePath atomically:YES]; //Write the file
+    self.imageView.image = chosenImage;
+    //NSLog(@"%@", info.description);
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [picker dismissViewControllerAnimated:YES completion:NULL];
 }
 
 @end
